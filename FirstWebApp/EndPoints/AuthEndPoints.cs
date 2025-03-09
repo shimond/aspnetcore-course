416fd166b2692da99e33f672eddac94215129af3
Shimon Dahan <shimond@any-techs.co.il>
2025-03-09 23:12:16 +0200

    Add user authentication with JWT in AuthEndPoints
    Updated the `AuthEndPoints` class to include a new `Login` method for user authentication, which validates credentials and generates a JWT token. Added a corresponding POST request in `FirstWebApp.http` for logging in, utilizing the token for subsequent product creation requests. Removed outdated login request from `FirstWebApp.http` for code cleanup.

--- FirstWebApp/EndPoints/AuthEndPoints.cs
+++ FirstWebApp/EndPoints/AuthEndPoints.cs
@@ -14,45 +14,59 @@ public static class AuthEndPoints
         paymentsGroup.MapPost("Login", Login).WithName(nameof(Login));
     }
 
+
     static async Task<Results<UnauthorizedHttpResult, Ok<LoginResponse>>> Login(LoginRequest request, IOptions<BearerInfo> bearerOptions, IAuthorizationService authorizationService)
     {
+        // Retrieve bearer information from options
         var bearerInformation = bearerOptions.Value;
+
+        // Validate user credentials
         var isValid = await authorizationService.IsUserValid(request.UserName, request.Password);
         if (!isValid)
         {
+            // Return unauthorized result if credentials are invalid
             return TypedResults.Unauthorized();
         }
 
+        // Create claims for the JWT token
         var claims = new List<Claim>
         {
             new Claim(ClaimTypes.Name, request.UserName),
             new Claim(JwtRegisteredClaimNames.Sub, request.UserName)
         };
 
+        // Add audience claims to the token
         var audiences = bearerInformation.ValidAudiences;
         foreach (var aud in audiences)
         {
             claims.Add(new Claim(JwtRegisteredClaimNames.Aud, aud));
         }
 
-
-        var signKeyInfo = bearerInformation.SigningKeys.First(x=>x.Issuer == bearerInformation.ValidIssuer);
+        // Retrieve signing key information
+        var signKeyInfo = bearerInformation.SigningKeys.First(x => x.Issuer == bearerInformation.ValidIssuer);
         var signKey = signKeyInfo.Value;
 
+        // Convert signing key from base64 string to byte array
         var keyMaterial = new byte[32];
         Convert.TryFromBase64String(signKey, keyMaterial, out var bytesWritten);
+
+        // Create signing credentials using the signing key
         var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(keyMaterial), SecurityAlgorithms.HmacSha256Signature);
 
+        // Create security token descriptor
         var token = new SecurityTokenDescriptor
         {
             SigningCredentials = signingCredentials,
-            Expires = DateTime.UtcNow.AddHours(1),
+            Expires = DateTime.UtcNow.AddHours(1), // Set token expiration time
             Issuer = bearerInformation.ValidIssuer,
             Subject = new ClaimsIdentity(claims)
         };
 
+        // Generate JWT token
         var tokenHandler = new JwtSecurityTokenHandler();
         var tokenString = tokenHandler.WriteToken(tokenHandler.CreateToken(token));
-        return TypedResults.Ok(new LoginResponse(tokenString)); 
+
+        // Return the generated token in the response
+        return TypedResults.Ok(new LoginResponse(tokenString));
     }
 }
